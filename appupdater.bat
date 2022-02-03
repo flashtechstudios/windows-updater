dc056ca806b6e66dc244713fadaabde3e168c4d3
Devesh3293 <96462198+Devesh3293@users.noreply.github.com>
2022-02-03 09:26:41 +0530

    Create appupdater.bat

---
+++ appupdater.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ============================
+echo APP UPDATER
+echo ============================
+echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps. If it throws an error then it probably means that you don't have winget. Learn more about winget: https://github.com/microsoft/winget-cli
+echo ============================
+echo I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY RUNNING THIS SCRIPT. USE AT YOUR RISK!
+echo ============================
+echo Do you want to update?
+CHOICE /C YNC /M "Press Y for Yes, N for No or C for Cancel."
+echo Retreiving list of apps to update:
+winget upgrade
+echo Updating your apps....
+winget upgrade --all
+echo Done!
+pause
