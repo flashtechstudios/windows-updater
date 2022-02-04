76949d26af215ed8df6e39e2c7b1b3db258bd435
Devesh3293 <96462198+Devesh3293@users.noreply.github.com>
2022-02-04 09:47:33 +0530

    Update appupdater.bat

--- appupdater.bat
+++ appupdater.bat
@@ -2,9 +2,10 @@
 echo ============================
 echo APP UPDATER
 echo ============================
-echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps. If it throws an error then it probably means that you don't have winget. Learn more about winget: https://github.com/microsoft/winget-cli
+echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps. If it throws an error then it probably means that you don't have winget. echo Learn more about winget: https://github.com/microsoft/winget-cli.
 echo ============================
-echo I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY RUNNING THIS SCRIPT. USE AT YOUR RISK!
+echo I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY RUNNING THIS SCRIPT.
+echo USE AT YOUR RISK!
 echo ============================
 echo Retreiving list of apps to update:
 winget upgrade
