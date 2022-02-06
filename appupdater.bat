0caa43f0c0b346699997dc2f056837fec01feac4
Devesh3293 <96462198+Devesh3293@users.noreply.github.com>
2022-02-07 00:41:52 +0530

    Update appupdater.bat

--- appupdater.bat
+++ appupdater.bat
@@ -1,6 +1,15 @@
 @echo off
 echo ============================
-echo APP UPDATER
+
+echo "           _____  _____    _    _ _____  _____       _______ ______ _____  ";
+echo "     /\   |  __ \|  __ \  | |  | |  __ \|  __ \   /\|__   __|  ____|  __ \ ";
+echo "    /  \  | |__) | |__) | | |  | | |__) | |  | | /  \  | |  | |__  | |__) |";
+echo "   / /\ \ |  ___/|  ___/  | |  | |  ___/| |  | |/ /\ \ | |  |  __| |  _  / ";
+echo "  / ____ \| |    | |      | |__| | |    | |__| / ____ \| |  | |____| | \ \ ";
+echo " /_/    \_\_|    |_|       \____/|_|    |_____/_/    \_\_|  |______|_|  \_\";
+echo "                                                                           ";
+echo "                                                                           ";
+
 echo ============================
 echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps. If it throws an error then it probably means that you don't have winget. Learn more about winget: https://github.com/microsoft/winget-cli.
 echo ============================
