6f50f0a5ad93a04bcdf0c19893426c3c9afcea46
Devesh3293 <96462198+themoonmeetsthesun@users.noreply.github.com>
2022-03-13 21:46:57 +0530

    -a

--- appupdater.bat
+++ appupdater.bat
@@ -11,7 +11,7 @@ echo "                                                                          
 echo "                                                                           ";
 
 echo ============================
-echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps. If it throws an error then it probably means that you don't have winget. Learn more about winget: https://github.com/microsoft/winget-cli.
+echo ONLY WORKS ON THE MOST RECENT VERSIONS OF WINDOWS 10! Update your system to the latest version for the most reliable experience. This script basically utilises winget in the background and updates (almost!) all your apps.
 echo ============================
 echo I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY RUNNING THIS SCRIPT.
 echo USE AT YOUR RISK!
