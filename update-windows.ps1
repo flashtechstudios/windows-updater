67e3fb5e7b3abfb368d25e48b330afb48bccc21c
Devesh3293 <96462198+themoonmeetsthesun@users.noreply.github.com>
2022-03-17 19:47:58 +0530

    -a

--- update-windows.ps1
+++ update-windows.ps1
@@ -1,6 +1,22 @@
+write-host "
+           _                       _       _            
+          (_)                     | |     | |           
+ __      ___ _ __  _   _ _ __   __| | __ _| |_ ___ _ __ 
+ \ \ /\ / / | '_ \| | | | '_ \ / _` |/ _` | __/ _ \ '__|
+  \ V  V /| | | | | |_| | |_) | (_| | (_| | ||  __/ |   
+   \_/\_/ |_|_| |_|\__,_| .__/ \__,_|\__,_|\__\___|_|   
+                        | |                             
+                        |_|                             
+"
 write-host "Updating everything...."
-write-host "Updating apps..."
+write-host "Upgrading apps..."
+write-host "Listing apps to upgrade"
+winget upgrade
+Write-Host "upgrading..."
 winget upgrade --all
 write-host "Updating Windows...."
+write-host "Listing updates"
+get-windowsupdate
+Write-Host "updating...."
 install-windowsupdate
 write-host "Done!"
